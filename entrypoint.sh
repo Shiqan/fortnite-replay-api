4063a57b92d01c9bff878e40306244c535cf8cfb
Shiqan <ferron.saan@live.nl>
2019-03-27 23:02:51 +0100

    add to docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-exec gunicorn --chdir project app:app --keep-alive 60 --bind 127.0.0.1:5000 --workers 5
+# run migrations
+while true; do
+    python manage.py db upgrade
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    >&2 echo "Upgrade command failed, retrying in 5 secs..."
+    sleep 5
+done
+
+exec gunicorn --chdir project app:app --keep-alive 60 --bind 0.0.0.0:5000 --workers 2
